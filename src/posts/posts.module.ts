91566751bd9458506fa403679655b23e0c172a9e
LastNyx <benediktus0804@gmail.com>
2022-06-09 21:31:14 +0700

    fix src

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { TagsService } from 'src/tags/tags.service';
+import { TagsService } from '../tags/tags.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
 import { PostsRepository } from './posts.repository';
-import { ImagesRepository } from 'src/images/images.repository';
-import { LinksRepository } from 'src/links/links.repository';
-import { TagsRepository } from 'src/tags/tags.repository';
+import { ImagesRepository } from '../images/images.repository';
+import { LinksRepository } from '../links/links.repository';
+import { TagsRepository } from '../tags/tags.repository';
 import { AuthService } from 'src/auth/auth.service';
 import { UsersRepository } from 'src/auth/users.repository';
 import { SecretsRepository } from 'src/secrets/secrets.repository';
